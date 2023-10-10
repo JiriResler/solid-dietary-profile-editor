68e8bd1b5c9981e747841e79f54073a75159cf08
JiriResler <j.resler.29@gmail.com>
2023-10-10 10:10:09 +0200

    add formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useState } from 'react';
-import {
-  useSession,
-  LoginButton,
-  LogoutButton,
-} from "@inrupt/solid-ui-react";
+import { useState } from 'react'
+import { useSession, LoginButton, LogoutButton } from '@inrupt/solid-ui-react'
 
 function App() {
-  const { session } = useSession();
+  const { session } = useSession()
 
   const identityProviders = [
     'https://solidcommunity.net/',
     'https://login.inrupt.com/',
     'https://inrupt.net/',
-    'https://solidweb.org/'
-  ];
+    'https://solidweb.org/',
+  ]
 
-  const [selectedProvider, setSelectedProvider] = useState(identityProviders[0]);
+  const [selectedProvider, setSelectedProvider] = useState(identityProviders[0])
 
   return (
     <BrowserRouter
@@ -24,37 +20,34 @@ function App() {
     >
       <Routes>
         <Route
-          path='/'
+          path="/"
           element={
             <>
               <h1>GH Pages deployment test</h1>
               <div>
-                isLoggedIn: {session.info.isLoggedIn == true ? "true" : "false"}
+                isLoggedIn: {session.info.isLoggedIn == true ? 'true' : 'false'}
               </div>
               <br />
               <div>
-                Your webID: {session.info.isLoggedIn == true ? session.info.webId : "Log in to see your WebID"}
+                Your webID:{' '}
+                {session.info.isLoggedIn == true
+                  ? session.info.webId
+                  : 'Log in to see your WebID'}
               </div>
               <br />
               <div>
                 <select
                   value={selectedProvider}
-                  onChange={e => setSelectedProvider(e.target.value)}
+                  onChange={(e) => setSelectedProvider(e.target.value)}
                 >
                   {identityProviders.map((providerUrl) => {
-                    return (
-                      <option key={providerUrl}>
-                        {providerUrl}
-                      </option>
-                    );
+                    return <option key={providerUrl}>{providerUrl}</option>
                   })}
-                </select>
-                {' '}
+                </select>{' '}
                 <LoginButton
                   oidcIssuer={selectedProvider}
                   redirectUrl={window.location.href}
-                />
-                {' '}
+                />{' '}
                 <LogoutButton />
               </div>
             </>
