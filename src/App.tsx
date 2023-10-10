cd24ecbe8eba3428094e5a99b70440025a3d799d
JiriResler <j.resler.29@gmail.com>
2023-10-10 21:37:38 +0200

    add Login and Profile components

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useState } from 'react'
-import { useSession, LoginButton, LogoutButton } from '@inrupt/solid-ui-react'
+import React from 'react'
+import Login from './Login'
+import Profile from './Profile'
 
-function App() {
+import { useSession } from '@inrupt/solid-ui-react'
+
+const App: React.FC = () => {
   const { session } = useSession()
 
-  const identityProviders = [
-    'https://solidcommunity.net/',
-    'https://login.inrupt.com/',
-    'https://inrupt.net/',
-    'https://solidweb.org/',
-  ]
-
-  const [selectedProvider, setSelectedProvider] = useState(identityProviders[0])
-
   return (
-    <BrowserRouter
-      basename={import.meta.env.DEV ? '/' : '/solid-dietary-profile-editor/'}
-    >
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <h1>GH Pages deployment test</h1>
-              <div>
-                isLoggedIn: {session.info.isLoggedIn == true ? 'true' : 'false'}
-              </div>
-              <br />
-              <div>
-                Your webID:{' '}
-                {session.info.isLoggedIn == true
-                  ? session.info.webId
-                  : 'Log in to see your WebID'}
-              </div>
-              <br />
-              <div>
-                <select
-                  value={selectedProvider}
-                  onChange={(e) => setSelectedProvider(e.target.value)}
-                >
-                  {identityProviders.map((providerUrl) => {
-                    return <option key={providerUrl}>{providerUrl}</option>
-                  })}
-                </select>{' '}
-                <LoginButton
-                  oidcIssuer={selectedProvider}
-                  redirectUrl={window.location.href}
-                />{' '}
-                <LogoutButton />
-              </div>
-            </>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      {!session.info.isLoggedIn && <Login />}
+      {session.info.isLoggedIn && <Profile />}
+    </>
   )
 }
 
---
+++ src/Profile.tsx
@@ -0,0 +1,143 @@
+import React, { useState } from 'react'
+import { useSession } from '@inrupt/solid-ui-react'
+import {
+  addStringNoLocale,
+  createSolidDataset,
+  createThing,
+  getPodUrlAll,
+  getSolidDataset,
+  getThingAll,
+  removeThing,
+  saveSolidDatasetAt,
+  setThing,
+  SolidDataset,
+} from '@inrupt/solid-client'
+// import { SCHEMA_INRUPT } from '@inrupt/vocab-common-rdf'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const Profile: React.FC = () => {
+  const { session } = useSession()
+
+  const [checkedAllergens, setCheckedAllergens] = useState(new Set<string>())
+
+  const allergens = [
+    'Celery',
+    'Gluten',
+    'Crustaceans',
+    'Eggs',
+    'Fish',
+    'Lupin',
+    'Milk',
+    'Molluscs',
+    'Mustard',
+    'Tree nuts',
+    'Peanuts',
+    'Sesame',
+    'Soya',
+    'Sulphites',
+  ]
+
+  // const diets = ["vegan", "vegetarian", "low-carb", "keto", "raw"];
+
+  async function handleWrite() {
+    const userWebId: string =
+      session.info.webId === undefined ? '' : session.info.webId
+    const podsUrls: string[] = await getPodUrlAll(userWebId, {
+      fetch: session.fetch,
+    })
+    const readingListUrl = `${podsUrls[0]}dietary-profile/my-profile`
+    let myReadingList: SolidDataset = setThing(
+      createSolidDataset(),
+      createThing(),
+    )
+
+    try {
+      // Attempt to retrieve the reading list in case it already exists.
+      let myReadingList = await getSolidDataset(readingListUrl, {
+        fetch: session.fetch,
+      })
+      // Clear the list to override the whole list
+      const items = getThingAll(myReadingList)
+      items.forEach((item) => {
+        myReadingList = removeThing(myReadingList, item)
+      })
+    } catch (error) {
+      if (typeof error.statusCode === 'number' && error.statusCode === 404) {
+        // if not found, create a new SolidDataset (i.e., the reading list)
+        myReadingList = createSolidDataset()
+      } else {
+        console.error(error.message)
+      }
+    }
+
+    let item = createThing({ name: userWebId })
+
+    for (const allergen of checkedAllergens) {
+      item = addStringNoLocale(
+        item,
+        'https://github.com/JiriResler/solid-choose-well-ontology/blob/main/choosewell#isAllergicTo',
+        allergen.toLowerCase(),
+      )
+    }
+
+    myReadingList = setThing(myReadingList, item)
+
+    await saveSolidDatasetAt(readingListUrl, myReadingList, {
+      fetch: session.fetch,
+    })
+
+    alert('Profile saved')
+  }
+
+  function handleAllergenClick(allergen: string) {
+    const newAllergenSet = new Set(checkedAllergens)
+
+    if (newAllergenSet.has(allergen)) {
+      newAllergenSet.delete(allergen)
+    } else {
+      newAllergenSet.add(allergen)
+    }
+
+    setCheckedAllergens(newAllergenSet)
+  }
+
+  return (
+    <>
+      <Container fluid>
+        <h3 className="mt-3">Select what you are allergic to</h3>
+        <Row className="w-25 mt-2">
+          {allergens.map((allergen) => (
+            <Col lg={6}>
+              <Form.Check
+                type="checkbox"
+                label={allergen}
+                checked={checkedAllergens.has(allergen)}
+                onChange={() => handleAllergenClick(allergen)}
+              />
+            </Col>
+          ))}
+        </Row>
+
+        {/* <h3>Select your diets</h3>
+      {diets.map(diet =>
+        <Form.Check
+          type="checkbox"
+          label={diet}
+          checked={false}
+          // onChange={() => handleAllergenClick(allergen)}
+        />
+      )} */}
+
+        <Button className="mt-3" onClick={() => handleWrite()}>
+          Save profile
+        </Button>
+      </Container>
+    </>
+  )
+}
+
+export default Profile
