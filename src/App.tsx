611e49b8dd8ad161cd4486c2ec4c21cf23d81025
JiriResler <j.resler.29@gmail.com>
2023-10-13 22:34:26 +0200

    add navbar to login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import Login from './Login'
-import Profile from './Profile'
-
-import { useSession } from '@inrupt/solid-ui-react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Navbar from 'react-bootstrap/Navbar'
 
 const App: React.FC = () => {
-  const { session } = useSession()
-
   return (
-    <>
-      {!session.info.isLoggedIn && <Login />}
-      {session.info.isLoggedIn && <Profile />}
-    </>
+    <Container fluid className="bg-success">
+      <Row>
+        <Navbar bg="primary" data-bs-theme="dark">
+          <Navbar.Brand href="#home" className="ms-3 fs-6">
+            About
+          </Navbar.Brand>
+        </Navbar>
+      </Row>
+
+      <Row className="position-absolute top-50 start-50 translate-middle">
+        Body
+      </Row>
+
+      <Row className="position-absolute bottom-0 end-0 me-auto">
+        <select>
+          <option value="selectLanguage">Select language</option>
+          <option value="option">Option</option>
+        </select>
+      </Row>
+    </Container>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { SessionProvider } from '@inrupt/solid-ui-react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SessionProvider sessionId="session-id">
-      <App />
-    </SessionProvider>
+    <App />
   </React.StrictMode>,
 )
