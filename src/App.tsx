9654882695490dadb76487f8a96d2a7a35d99788
JiriResler <j.resler.29@gmail.com>
2023-10-15 20:14:09 +0200

    update solid button
    add solid logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,54 @@ import Navbar from 'react-bootstrap/Navbar'
 import Stack from 'react-bootstrap/Stack'
 import Button from 'react-bootstrap/Button'
 import './styles/SolidButton.css'
+import Col from 'react-bootstrap/Col'
 
 const App: React.FC = () => {
   return (
-    <Container fluid className="bg-success">
-      <Row>
-        <Navbar bg="primary" data-bs-theme="dark">
-          <Navbar.Brand href="#home" className="ms-3 fs-6">
-            About
-          </Navbar.Brand>
-        </Navbar>
-      </Row>
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Navbar.Brand href="#home" className="ms-3 fs-6">
+          About
+        </Navbar.Brand>
+      </Navbar>
 
-      <Row className="position-absolute top-50 start-50 translate-middle">
-        <Stack gap={3}>
-          <h2>Dietary profile editor</h2>
-          <h6>Select who you want to trust with your personal data</h6>
-          <Button className="solid-button">Trust Solid</Button>
-          <Button variant="outline-secondary">Trust Google</Button>
-          <a
-            href="/home"
-            className="text-center link-underline link-underline-opacity-0"
-          >
-            What is the difference?
-          </a>
-        </Stack>
-      </Row>
+      <Container>
+        <Row className="position-absolute top-50 start-50 translate-middle">
+          <Col>
+            <Stack gap={3}>
+              <h2>Dietary profile editor</h2>
+              <h6>Select who you want to trust with your personal data</h6>
+              <Row className="align-items-center">
+                <Col xs={2}>
+                  <img
+                    src="../resources/logo_solid.svg"
+                    alt="Solid project logo"
+                    style={{ width: '150%' }}
+                  />
+                </Col>
+                <Col>
+                  <Button className="solid-button">Trust Solid</Button>
+                </Col>
+              </Row>
+              <Button variant="outline-secondary">Trust Google</Button>
+              <a
+                href="/home"
+                className="text-center link-underline link-underline-opacity-0"
+              >
+                What is the difference?
+              </a>
+            </Stack>
+          </Col>
+        </Row>
 
-      <Row className="position-absolute bottom-0 end-0 me-auto">
-        <select>
-          <option value="selectLanguage">Select language</option>
-          <option value="option">Option</option>
-        </select>
-      </Row>
-    </Container>
+        <Row className="position-absolute bottom-0 end-0 me-auto">
+          <select>
+            <option value="selectLanguage">Select language</option>
+            <option value="option">Option</option>
+          </select>
+        </Row>
+      </Container>
+    </>
   )
 }
 
