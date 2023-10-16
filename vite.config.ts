7545b647d7e81de2ab58daaaa9078bd94b3b654d
JiriResler <j.resler.29@gmail.com>
2023-10-16 08:12:06 +0200

    simplify vite config file
    remove unnecessary function

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/solid-dietary-profile-editor/',
-    test: {
-      environment: 'jsdom',
-    },
-  }
+const config = {
+  plugins: [react()],
+  base: '/solid-dietary-profile-editor/',
+  test: {
+    environment: 'jsdom',
+  },
+}
 
-  return config
-})
+export default config
