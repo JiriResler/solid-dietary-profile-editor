c1cee3b62b207330ddf888ce8f29fffaed5bfec0
JiriResler <j.resler.29@gmail.com>
2023-10-22 13:28:31 +0200

    move BrowserRouter to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
 import Contact from './Contact'
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/solid-dietary-profile-editor/" element={<Home />} />
-        <Route path="/solid-dietary-profile-editor/about" element={<About />} />
-        <Route
-          path="/solid-dietary-profile-editor/contact"
-          element={<Contact />}
-        />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/solid-dietary-profile-editor/" element={<Home />} />
+          <Route
+            path="/solid-dietary-profile-editor/about"
+            element={<About />}
+          />
+          <Route
+            path="/solid-dietary-profile-editor/contact"
+            element={<Contact />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
