37a2d9523341bcb24537549489f522d8d5f135c4
JiriResler <j.resler.29@gmail.com>
2023-10-22 11:05:04 +0200

    add firebase package

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCJAA5IFz8DSQBE54fO6aTKC8BVCs-zssU',
+  authDomain: 'profile-and-personalized-menus.firebaseapp.com',
+  projectId: 'profile-and-personalized-menus',
+  storageBucket: 'profile-and-personalized-menus.appspot.com',
+  messagingSenderId: '740463798793',
+  appId: '1:740463798793:web:31d8f199a5df8f75bfe6d8',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
