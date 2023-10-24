a0d39905f9f5398098a9c16733022860a6e0f7b7
JiriResler <j.resler.29@gmail.com>
2023-10-24 13:30:54 +0200

    add envelope icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import { Routes, Route } from 'react-router-dom'
-import { BrowserRouter } from 'react-router-dom'
-import Home from './Home'
-import About from './About'
-import { LoginMethodContext } from './LoginMethodContext'
-// import { useContext } from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Navbar from 'react-bootstrap/Navbar'
+import Stack from 'react-bootstrap/Stack'
+import Button from 'react-bootstrap/Button'
+import './styles/LoginScreen.css'
+import Col from 'react-bootstrap/Col'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <LoginMethodContext.Provider value={'default-context'}>
-      <BrowserRouter
-        basename={import.meta.env.DEV ? '/' : '/solid-dietary-profile-editor/'}
-      >
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </LoginMethodContext.Provider>
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Navbar.Brand href="#home" className="ms-3 fs-6">
+          About
+        </Navbar.Brand>
+      </Navbar>
+
+      <Container>
+        <Row className="position-absolute top-50 start-50 translate-middle ">
+          <Col>
+            <Stack gap={3} className="text-center">
+              <h2>Dietary profile editor</h2>
+              <h6>Sign in with</h6>
+              <Row className="align-items-center">
+                <Col xs={3}>
+                  <img src="images/logo_solid.svg" alt="Solid project logo" className="login-provider-icon"/>
+                </Col>
+                <Col className="text-start">
+                  <Button className="solid-button text-start">
+                    Solid WebID
+                  </Button>
+                </Col>
+              </Row>
+              <div>or</div>
+              <Row className="align-items-center">
+                <Col xs={3}>
+                  <img src="images/envelope-at.svg" alt="Envelope icon" className="login-provider-icon" />
+                </Col>
+                <Col className="text-start">
+                  <Button className="google-button text-start">
+                    Email and password
+                  </Button>
+                </Col>
+              </Row>
+              <a
+                href="/home"
+                className="text-center link-underline link-underline-opacity-0"
+              >
+                What is the difference?
+              </a>
+            </Stack>
+          </Col>
+        </Row>
+
+        <Row className="position-absolute bottom-0 end-0 me-auto">
+          <select>
+            <option value="selectLanguage">Select language</option>
+            <option value="option">Option</option>
+          </select>
+        </Row>
+      </Container>
+    </>
   )
 }
 
