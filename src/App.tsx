9456ee366346c07849e3d3305674e9cd6754154d
JiriResler <j.resler.29@gmail.com>
2023-10-24 08:47:29 +0200

    add working test context

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,30 @@ import { Routes, Route } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
+import { ImageSizeContext } from './Context.js'
+import { useContext } from 'react'
 
 function App() {
   return (
-    <BrowserRouter
-      basename={import.meta.env.DEV ? '/' : '/solid-dietary-profile-editor/'}
-    >
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <ImageSizeContext.Provider value={1000}>
+      <BrowserRouter
+        basename={import.meta.env.DEV ? '/' : '/solid-dietary-profile-editor/'}
+      >
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <PlaceImage />
+        </div>
+      </BrowserRouter>
+    </ImageSizeContext.Provider>
   )
+}
+
+function PlaceImage() {
+  const imageSize = useContext(ImageSizeContext)
+  return <p>{imageSize}</p>
 }
 
 export default App
