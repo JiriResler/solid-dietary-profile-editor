963a8f7bf6f99f42e7cbccdbf73048d4e4d1305a
JiriResler <j.resler.29@gmail.com>
2023-10-27 16:49:16 +0200

    add bootstrap select form

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,29 @@ import About from './About'
 import SignInMethodComparison from './SignInMethodComparison'
 import './styles/LoginScreen.css'
 import { IntlProvider } from 'react-intl'
+// import { useState } from 'react'
 
-const sk_messages = { 
+const sk_messages = {
   app_name: 'Editor diétneho profilu',
   about_application: 'O aplikácii',
   sign_in_with: 'Prihlásiť sa cez',
   or: 'alebo',
   email_and_password: 'Email a heslo',
-  what_is_the_difference: 'Aký je v tom rozdiel?'
- }
+  what_is_the_difference: 'Aký je v tom rozdiel?',
+}
+
+// const cz_messages = {
+//   app_name: 'Editor dietního profilu',
+//   about_application: 'O aplikaci',
+//   sign_in_with: 'Přihlásit se přes',
+//   or: 'nebo',
+//   email_and_password: 'Email a heslo',
+//   what_is_the_difference: 'Jaký je v tom rozdíl?',
+// }
 
 const App: React.FC = () => {
-  const selectedLanguage = 'en'
   return (
-    <IntlProvider
-      messages={sk_messages}
-      locale={selectedLanguage}
-      defaultLocale="en"
-    >
+    <IntlProvider messages={sk_messages} locale={''} defaultLocale="en">
       <BrowserRouter
         basename={import.meta.env.DEV ? '/' : '/solid-dietary-profile-editor/'}
       >
--- src/Home.tsx
+++ src/Home.tsx
@@ -6,6 +6,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Stack from 'react-bootstrap/Stack'
 import Button from 'react-bootstrap/Button'
 import { FormattedMessage } from 'react-intl'
+import Form from 'react-bootstrap/Form'
 
 function Home() {
   return (
@@ -13,33 +14,30 @@ function Home() {
       <Navbar bg="primary" data-bs-theme="dark">
         <Navbar.Brand className="ms-3 fs-6">
           <Link className="navbar-link" to="about">
-          <FormattedMessage
-            id="about_application"
-            defaultMessage={'About'}
-          />
+            <FormattedMessage id="about_application" defaultMessage={'About'} />
           </Link>
         </Navbar.Brand>
       </Navbar>
 
       <h1 className="text-center app-name-row translate-middle">
-      <FormattedMessage
-            id="app_name"
-            defaultMessage={'Dietary profile editor'}
-          />
+        <FormattedMessage
+          id="app_name"
+          defaultMessage={'Dietary profile editor'}
+        />
       </h1>
 
       <Container>
-        <Row className="sign-in-row position-absolute top-50 start-50 translate-middle ">
+        <Row className="border sign-in-row position-absolute top-50 start-50 translate-middle ">
           <Col>
             <Stack gap={2} className="text-center">
               <Row className="pb-2">
                 <Col>
                   <h5>
-                    <FormattedMessage 
+                    <FormattedMessage
                       id="sign_in_with"
                       defaultMessage={'Sign in with'}
                     />
-                    </h5>
+                  </h5>
                 </Col>
               </Row>
               <Row className="align-items-center">
@@ -58,10 +56,8 @@ function Home() {
               </Row>
               <Row>
                 <Col>
-                <FormattedMessage 
-                      id="or"
-                      defaultMessage={'or'}
-                    /></Col>
+                  <FormattedMessage id="or" defaultMessage={'or'} />
+                </Col>
               </Row>
               <Row className="align-items-center">
                 <Col xs={3}>
@@ -73,7 +69,7 @@ function Home() {
                 </Col>
                 <Col className="text-start">
                   <Button className="text-start sign-in-with-email-button">
-                  <FormattedMessage 
+                    <FormattedMessage
                       id="email_and_password"
                       defaultMessage={'Email and password'}
                     />
@@ -86,7 +82,7 @@ function Home() {
                     className="sign-in-method-difference-link"
                     to="sign-in-methods-comparison"
                   >
-                    <FormattedMessage 
+                    <FormattedMessage
                       id="what_is_the_difference"
                       defaultMessage={'What is the difference?'}
                     />
@@ -99,10 +95,14 @@ function Home() {
       </Container>
 
       <div className="position-absolute bottom-0 end-0 me-3 mb-3">
-        <select>
-          <option value="selectLanguage">Select language</option>
-          <option value="option">Option</option>
-        </select>
+        <Form.Select
+
+        // value={}
+        // onChange={(e) => f(e.target.value)}
+        >
+          <option key="slovak">Slovensky</option>
+          <option key="czech">Česky</option>
+        </Form.Select>
       </div>
     </>
   )
