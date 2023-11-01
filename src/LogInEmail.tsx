687393170d12ddf71baafbd467a7c957ff140ef5
JiriResler <j.resler.29@gmail.com>
2023-11-01 11:21:35 +0100

    add log out button for firebase

--- src/LogInEmail.tsx
+++ src/LogInEmail.tsx
@@ -1,17 +1,19 @@
 import { Link } from 'react-router-dom'
 import { auth } from './firebase'
 import { signInWithEmailAndPassword } from 'firebase/auth'
+import { signOut } from 'firebase/auth'
 import { useState } from 'react'
 
 function LogInEmail() {
   const [loginEmail, setLoginEmail] = useState('')
   const [loginPassword, setLoginPassword] = useState('')
 
+  //   const auth = getAuth()
+
   const loginWithUsernameAndPassword = async (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
     e.preventDefault()
-
     try {
       await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
       alert('Login successful')
@@ -20,8 +22,21 @@ function LogInEmail() {
     }
   }
 
+  async function logOut() {
+    try {
+      await signOut(auth)
+      alert('Log out successful')
+    } catch {
+      alert('Log out failed.')
+    }
+  }
+
   return (
     <div>
+      <p>is logged in: {auth.currentUser !== null ? 'true' : 'false'}</p>
+      <p>
+        auth.currentUser is {auth.currentUser === null ? 'null' : 'not null'}
+      </p>
       <h1>Log in with email and password</h1>
       <form>
         <input
@@ -43,6 +58,14 @@ function LogInEmail() {
           Log in
         </button>
       </form>
+      <br />
+      <button
+        onClick={() => {
+          void logOut()
+        }}
+      >
+        Log out
+      </button>
       <br />
       <Link to="/">Go back</Link>
     </div>
