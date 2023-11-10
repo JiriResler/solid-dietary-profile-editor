dde22f973be2d61a22974af3ec1902c53da10cfb
JiriResler <j.resler.29@gmail.com>
2023-11-10 14:54:00 +0100

    align controls to sign in with email

--- src/LogInEmail.tsx
+++ src/LogInEmail.tsx
@@ -1,44 +1,51 @@
-import { auth } from './firebase'
-import { signInWithEmailAndPassword } from 'firebase/auth'
-import { signOut } from 'firebase/auth'
-import { useState } from 'react'
+// import { auth } from './firebase'
+// import { signInWithEmailAndPassword } from 'firebase/auth'
+// import { signOut } from 'firebase/auth'
+// import { useState } from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Stack from 'react-bootstrap/Stack'
 
 function LogInEmail() {
-  const [loginEmail, setLoginEmail] = useState('new.profile@gmail.com')
-  const [loginPassword, setLoginPassword] = useState('123456')
+  // const [loginEmail, setLoginEmail] = useState('new.profile@gmail.com')
+  // const [loginPassword, setLoginPassword] = useState('123456')
 
   //   const auth = getAuth()
 
-  const loginWithUsernameAndPassword = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-  ) => {
-    e.preventDefault()
-    try {
-      await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-      alert('Login successful')
-    } catch {
-      alert('You entered a wrong username or password.')
-    }
-  }
+  // const loginWithUsernameAndPassword = async (
+  //   e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  // ) => {
+  //   e.preventDefault()
+  //   try {
+  //     await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+  //     alert('Login successful')
+  //   } catch {
+  //     alert('You entered a wrong username or password.')
+  //   }
+  // }
 
-  async function logOut() {
-    try {
-      await signOut(auth)
-      alert('Log out successful')
-    } catch {
-      alert('Log out failed.')
-    }
-  }
+  // async function logOut() {
+  //   try {
+  //     await signOut(auth)
+  //     alert('Log out successful')
+  //   } catch {
+  //     alert('Log out failed.')
+  //   }
+  // }
 
   return (
-    <div>
-      <h1>Sign In or Sign Up</h1>
+    <Stack gap={3} className="text-center">
+      <h4>Sign In or Sign Up</h4>
       <input placeholder="Email Address"></input>
       <button>CONTINUE</button>
-      <p>or</p>
-      <hr></hr>G <button>Sign in with Google</button> <br />F{' '}
-      <button>Sign in with Facebook</button>
-    </div>
+      <span>or</span>
+      <div>
+        G <button>Sign in with Google</button>
+      </div>
+      <div>
+        F <button>Sign in with Facebook</button>
+      </div>
+    </Stack>
   )
 }
 
