270a202597bfb7e7efa5af4ff8a81c4a130a012d
JiriResler <j.resler.29@gmail.com>
2023-11-10 19:12:00 +0100

    add email sign in controls

--- src/LogInEmail.tsx
+++ src/LogInEmail.tsx
@@ -1,7 +1,7 @@
 // import { auth } from './firebase'
 // import { signInWithEmailAndPassword } from 'firebase/auth'
 // import { signOut } from 'firebase/auth'
-// import { useState } from 'react'
+import { useState } from 'react'
 // import Row from 'react-bootstrap/Row'
 // import Col from 'react-bootstrap/Col'
 import Stack from 'react-bootstrap/Stack'
@@ -9,10 +9,7 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 function LogInEmail() {
-  // const [loginEmail, setLoginEmail] = useState('new.profile@gmail.com')
-  // const [loginPassword, setLoginPassword] = useState('123456')
-
-  //   const auth = getAuth()
+  const [signInState, setSignInState] = useState('email')
 
   // const loginWithUsernameAndPassword = async (
   //   e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
@@ -36,23 +33,50 @@ function LogInEmail() {
   // }
 
   return (
-    <Stack gap={3} className="text-center">
-      <h4>Sign In or Sign Up</h4>
-      <Form>
-        <Form.Group className="" controlId="exampleForm.ControlInput1">
-          <Form.Control type="email" placeholder="Email Address" />
-        </Form.Group>
-      </Form>
+    <>
+      {signInState === 'email' && (
+        <Stack gap={3} className="text-center">
+          <h4>Sign In or Sign Up</h4>
+          <Form>
+            <Form.Group className="" controlId="exampleForm.ControlInput1">
+              <Form.Control type="email" placeholder="Email Address" />
+            </Form.Group>
+          </Form>
 
-      <Button >CONTINUE</Button>
-      <span>or</span>
-      <div>
-        G <button>Sign in with Google</button>
-      </div>
-      <div>
-        F <button>Sign in with Facebook</button>
-      </div>
-    </Stack>
+          <Button
+            onClick={() => {
+              setSignInState('password')
+            }}
+          >
+            CONTINUE
+          </Button>
+          <span>or</span>
+          <div>
+            G <button>Sign in with Google</button>
+          </div>
+          <div>
+            F <button>Sign in with Facebook</button>
+          </div>
+        </Stack>
+      )}
+
+      {signInState === 'password' && (
+        <div>
+          <h4>Sign in</h4>
+          <input placeholder="Email address"></input>
+          <input placeholder="Password"></input>
+          <p>Forgot Your Password</p>
+          <button>SIGN IN</button> <br />
+          <button
+            onClick={() => {
+              setSignInState('email')
+            }}
+          >
+            Back to email
+          </button>
+        </div>
+      )}
+    </>
   )
 }
 
