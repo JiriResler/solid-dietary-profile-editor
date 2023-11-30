69f8a8000e0bf264c3b7b48456c4bdcd090d3693
JiriResler <j.resler.29@gmail.com>
2023-11-30 19:51:21 +0100

    add loading screen when redirecting with solid

--- src/components/RouterWrapper.tsx
+++ src/components/RouterWrapper.tsx
@@ -3,9 +3,9 @@ import { useSession } from '@inrupt/solid-ui-react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from '../firebase'
 import Profile from './Profile'
-import About from './About'
+// import About from './About'
 import LoginScreen from './LoginScreen'
-import LogInSolid from './LogInSolid'
+// import LogInSolid from './LogInSolid'
 
 function RouterWrapper() {
   const { session, sessionRequestInProgress } = useSession()
@@ -14,8 +14,6 @@ function RouterWrapper() {
   const userIsLoggedIn = session.info.isLoggedIn || user !== null
 
   function profileIfAuthenticated() {
-    const userIsLoggedIn = session.info.isLoggedIn || user !== null
-
     if (userIsLoggedIn) {
       if (session.info.isLoggedIn) {
         return <Profile selectedSignInMethod="solid" />
@@ -28,24 +26,22 @@ function RouterWrapper() {
   }
 
   function loginIfNotAuthenticated() {
-    const userIsLoggedOut = !session.info.isLoggedIn && user === null
-
-    if (userIsLoggedOut) {
+    if (!userIsLoggedIn) {
       return <LoginScreen />
     } else {
       return <Navigate to="/" />
     }
   }
 
-  function aboutIfNotAuthenticated() {
-    const userIsLoggedOut = !session.info.isLoggedIn && user === null
+  // function aboutIfNotAuthenticated() {
+  //   if (!userIsLoggedIn) {
+  //     return <About />
+  //   } else {
+  //     return <Navigate to="/" />
+  //   }
+  // }
 
-    if (userIsLoggedOut) {
-      return <About />
-    } else {
-      return <Navigate to="/" />
-    }
-  }
+  // while (sessionRequestInProgress){}
 
   if (sessionRequestInProgress) {
     return <h1>Loading...</h1>
@@ -55,28 +51,10 @@ function RouterWrapper() {
     <BrowserRouter
       basename={import.meta.env.DEV ? '/' : '/solid-dietary-profile-editor/'}
     >
-      {userIsLoggedIn && (
-        <Routes>
-          <Route path="/" element={<Profile selectedSignInMethod="solid" />} />
-          <Route
-            path="/login"
-            element={<Profile selectedSignInMethod="solid" />}
-          />
-        </Routes>
-      )}
-
-      {!userIsLoggedIn && (
-        <Routes>
-          <Route
-            path="/"
-            element={<LogInSolid setLoginWithSolid={() => {}} />}
-          />
-          <Route
-            path="/login"
-            element={<LogInSolid setLoginWithSolid={() => {}} />}
-          />
-        </Routes>
-      )}
+      <Routes>
+        <Route path="/" element={profileIfAuthenticated()} />
+        <Route path="/login" element={loginIfNotAuthenticated()} />
+      </Routes>
     </BrowserRouter>
   )
 }
