4b2fac3cb50fa17c707787c7a79ec55acd60442a
JiriResler <j.resler.29@gmail.com>
2023-12-07 14:35:36 +0100

    add mustard icon

--- src/components/SelectAllergens.tsx
+++ src/components/SelectAllergens.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Stack from 'react-bootstrap/Stack'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+// import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import './SelectAllergens.css'
 
@@ -15,9 +15,15 @@ const SelectAllergens: React.FC = () => {
           A number in brackets is the number of the allergen given by the law.
         </p>
         <Stack direction="horizontal" gap={2}>
-          <input type="checkbox"></input>
-          <div>Allergen (x)</div>
-          <div>i</div>
+          <Form.Check type={'checkbox'} id={'id'} />
+          <div>Gluten (2)</div>
+          <img src="images/Gluten.svg" alt="" className="allergen-icon" />
+        </Stack>
+
+        <Stack direction="horizontal" gap={2}>
+          <Form.Check type={'checkbox'} id={'id'} />
+          <div>Mustard (x)</div>
+          <img src="images/Mustard.svg" alt="" className="allergen-icon" />
         </Stack>
       </Stack>
       <div className="mt-5">1/3</div>
