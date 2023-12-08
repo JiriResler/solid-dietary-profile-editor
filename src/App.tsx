500d194d8ba673f138c368afd20a5eea48d4b9a0
JiriResler <j.resler.29@gmail.com>
2023-12-08 11:34:31 +0100

    add bootstrap container wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { SessionProvider } from '@inrupt/solid-ui-react'
 import IntlProviderWrapper from './components/IntlProviderWrapper'
 import RouterWrapper from './components/RouterWrapper'
+import { Container as BootstrapContainer } from 'react-bootstrap'
 
 const App: React.FC = () => {
   return (
     <SessionProvider>
       <IntlProviderWrapper>
-        <RouterWrapper />
+        <BootstrapContainer>
+          <RouterWrapper />
+        </BootstrapContainer>
       </IntlProviderWrapper>
     </SessionProvider>
   )
