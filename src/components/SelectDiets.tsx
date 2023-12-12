626e63c30d3ac24eb40731de2764b5b694de21c3
JiriResler <j.resler.29@gmail.com>
2023-12-12 22:11:36 +0100

    add async select with filtering

--- src/components/SelectDiets.tsx
+++ src/components/SelectDiets.tsx
@@ -1,5 +1,5 @@
 import Form from 'react-bootstrap/Form'
-import Select from 'react-select'
+import AsyncSelect from 'react-select/async'
 import { useState } from 'react'
 
 type DietOption = { label: string; value: number }
@@ -19,24 +19,44 @@ const SelectDiets: React.FC = () => {
     setSelectedDiets(dietArray)
   }
 
+  const filterDiets = (inputValue: string) => {
+    return [
+      { value: 1, label: 'aaa' },
+      { value: 2, label: 'abb' },
+      { value: 3, label: 'abc' },
+      { value: 4, label: 'aba' },
+    ].filter((i) => i.label.toLowerCase().includes(inputValue.toLowerCase()))
+  }
+
+  async function fetchDiets(inputValue: string) {
+    const response = await fetch(
+      'https://raw.githubusercontent.com/JiriResler/solid-choose-well-ontology/main/diets_data.json',
+    )
+    const movies = await response.json()
+    console.log(movies)
+
+    return filterDiets(inputValue)
+  }
+
   return (
     <>
+      <ul>
+        {selectedDiets.map((diet) => {
+          return <li key={diet.value}>{diet.label}</li>
+        })}
+      </ul>
       <h1>2. Which diets are you on?</h1>
       <Form.Check type={'checkbox'} id={'id'} label="Vegan" />
       <Form.Check type={'checkbox'} id={'id'} label="Vegetarian" />
       <h3>Other diets</h3>
-      <Select
-        options={[
-          { value: 1, label: 'aaa' },
-          { value: 2, label: 'abb' },
-          { value: 3, label: 'abc' },
-          { value: 4, label: 'aba' },
-        ]}
-        value={selectedDiets}
+      <AsyncSelect
+        loadOptions={fetchDiets}
+        defaultOptions
+        isMulti
+        // value={selectedDiets}
         onChange={(value) => {
           handleSelectChange(value)
         }}
-        isMulti
         openMenuOnClick={false}
         components={SelectComponents}
         placeholder="Search for a diet"
