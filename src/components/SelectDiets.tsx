251d122c4874180343783320f48366634c6ada45
JiriResler <j.resler.29@gmail.com>
2023-12-13 16:41:51 +0100

    fetch diets on button click

--- src/components/SelectDiets.tsx
+++ src/components/SelectDiets.tsx
@@ -1,5 +1,5 @@
 import Form from 'react-bootstrap/Form'
-import AsyncSelect from 'react-select/async'
+import Select from 'react-select'
 import { useState } from 'react'
 
 type DietOption = { label: string; value: number }
@@ -15,31 +15,36 @@ const SelectDiets: React.FC = () => {
     [],
   )
 
+  const [dietOptions, setDietOptions] = useState<DietOption[]>([])
+
   function handleSelectChange(dietArray: ReadonlyArray<DietOption>) {
     setSelectedDiets(dietArray)
   }
 
-  const filterDiets = (inputValue: string) => {
-    return [
-      { value: 1, label: 'aaa' },
-      { value: 2, label: 'abb' },
-      { value: 3, label: 'abc' },
-      { value: 4, label: 'aba' },
-    ].filter((i) => i.label.toLowerCase().includes(inputValue.toLowerCase()))
-  }
-
-  async function fetchDiets(inputValue: string) {
+  async function fetchDiets() {
     const response = await fetch(
       'https://raw.githubusercontent.com/JiriResler/solid-choose-well-ontology/main/diets_data.json',
     )
     const movies = await response.json()
     console.log(movies)
 
-    return filterDiets(inputValue)
+    setDietOptions([
+      { value: 1, label: 'aaa' },
+      { value: 2, label: 'abb' },
+      { value: 3, label: 'abc' },
+      { value: 4, label: 'aba' },
+    ])
   }
 
   return (
     <>
+      <button
+        onClick={() => {
+          void fetchDiets()
+        }}
+      >
+        Fetch diets
+      </button>
       <ul>
         {selectedDiets.map((diet) => {
           return <li key={diet.value}>{diet.label}</li>
@@ -49,9 +54,9 @@ const SelectDiets: React.FC = () => {
       <Form.Check type={'checkbox'} id={'id'} label="Vegan" />
       <Form.Check type={'checkbox'} id={'id'} label="Vegetarian" />
       <h3>Other diets</h3>
-      <AsyncSelect
-        loadOptions={fetchDiets}
-        defaultOptions
+      <Select
+        options={dietOptions}
+        value={selectedDiets}
         isMulti
         // value={selectedDiets}
         onChange={(value) => {
