a68a891bc555d1c69dfdd13358f82af8b74ee5d9
JiriResler <j.resler.29@gmail.com>
2024-02-11 11:54:01 +0100

    add allergen description

--- src/components/CreateProfile/SelectAllergens.tsx
+++ src/components/CreateProfile/SelectAllergens.tsx
@@ -8,6 +8,11 @@ import { useState } from 'react'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 
+interface AllergenDescription extends Allergen {
+  description: string
+  imageUrl: string
+}
+
 type Props = {
   allergenArray: Allergen[]
   currentStep: number
@@ -21,9 +26,11 @@ const SelectAllergens: React.FC<Props> = ({
   selectedAllergens,
   setSelectedAllergens,
 }) => {
-  const [showAllergenDetail, setShowAllergenDetail] = useState(false)
+  const [showAllergenDescriptionModal, setShowAllergenDescriptionModal] =
+    useState(false)
 
-  const [allergenInfo, setAllergenInfo] = useState<Allergen | null>(null)
+  const [allergenDescription, setAllergenDescription] =
+    useState<AllergenDescription | null>(null)
 
   function handleCheckboxOnChange(allergen: Allergen) {
     const newAllergenSet = new Set(selectedAllergens)
@@ -35,6 +42,21 @@ const SelectAllergens: React.FC<Props> = ({
     }
 
     setSelectedAllergens(newAllergenSet)
+  }
+
+  function displayAllergenDescription(allergen: Allergen) {
+    const allergenDescription: AllergenDescription = {
+      ...allergen,
+      description:
+        'Celery (Apium graveolens) is a marshland plant in the family Apiaceae that has been cultivated as a vegetable since antiquity. Celery has a long fibrous stalk tapering into leaves. Depending on location and cultivar, either its stalks, leaves or hypocotyl are eaten and used in cooking. Celery seed powder is used as a spice.',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/a/a2/Celery_2.jpg',
+    }
+
+    // todo: add loading animation
+    setShowAllergenDescriptionModal(true)
+
+    setAllergenDescription(allergenDescription)
   }
 
   return (
@@ -59,8 +81,7 @@ const SelectAllergens: React.FC<Props> = ({
                   src="images/info_icon.svg"
                   alt="information icon"
                   onClick={() => {
-                    setAllergenInfo(allergen)
-                    setShowAllergenDetail(true)
+                    displayAllergenDescription(allergen)
                   }}
                 />
               </Stack>
@@ -69,31 +90,39 @@ const SelectAllergens: React.FC<Props> = ({
         })}
       </Row>
 
-      {allergenInfo !== null && (
+      {allergenDescription !== null && (
         <Modal
           size="lg"
           centered
-          show={showAllergenDetail}
+          show={showAllergenDescriptionModal}
           onHide={() => {
-            setShowAllergenDetail(false)
+            setShowAllergenDescriptionModal(false)
           }}
         >
-          <Modal.Body>
-            {allergenInfo.label +
-              ' has number ' +
-              allergenInfo.menuLegendNumber}
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <div>{allergenDescription.label}</div>
+              <div>
+                Menu legend number: {allergenDescription.menuLegendNumber}
+              </div>
+            </Modal.Title>
+          </Modal.Header>
 
-            <div className="mt-3 text-end">
-              <Button
-                variant="secondary"
-                onClick={() => {
-                  setShowAllergenDetail(false)
-                }}
-              >
-                Close
-              </Button>
-            </div>
+          <Modal.Body>
+            {allergenDescription.description}
+            <img className="w-50" src={allergenDescription.imageUrl} />
           </Modal.Body>
+
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              onClick={() => {
+                setShowAllergenDescriptionModal(false)
+              }}
+            >
+              Close
+            </Button>
+          </Modal.Footer>
         </Modal>
       )}
     </>
