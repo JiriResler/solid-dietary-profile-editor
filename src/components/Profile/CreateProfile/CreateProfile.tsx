5613bc732c592118bded44d999aace5bbfe0c5dd
JiriResler <j.resler.29@gmail.com>
2024-04-24 10:09:30 +0200

    add MUI stepper component

--- src/components/Profile/CreateProfile/CreateProfile.tsx
+++ src/components/Profile/CreateProfile/CreateProfile.tsx
@@ -11,6 +11,10 @@ import { LoginMethod } from '../../loginMethodEnum'
 import SelectTastePreferences from './SelectTastePreferences'
 // import { saveProfileFirebase, saveProfileSolid } from './saveProfile'
 import { loadProfileCreationData } from './loadProfileCreationData'
+import Box from '@mui/material/Box'
+import Stepper from '@mui/material/Stepper'
+import Step from '@mui/material/Step'
+import StepLabel from '@mui/material/StepLabel'
 
 type Props = {
   loginMethod: LoginMethod
@@ -27,6 +31,12 @@ const CreateProfile: React.FC<Props> = ({
 
   // Number of steps in profile creation
   // const numberOfSteps = 3
+
+  const steps = [
+    'Select master blaster campaign settings',
+    'Create an ad group',
+    'Create an ad',
+  ]
 
   const [currentStep, setCurrentStep] = useState(startStep)
 
@@ -73,55 +83,63 @@ const CreateProfile: React.FC<Props> = ({
 
   return (
     <>
-      <div className="">
-        {currentStep === 0 && (
-          <Stack
-            gap={3}
-            className="welcome-screen-stack mt-5 text-center mx-auto"
+      <h1>{'<'}</h1>
+      <Box sx={{ width: '100%' }}>
+        <Stepper activeStep={1} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
+      {currentStep === 0 && (
+        <Stack
+          gap={3}
+          className="welcome-screen-stack mt-5 text-center mx-auto"
+        >
+          <h1>Welcome!</h1>
+          <div>
+            You can create a new profile or import an already existing one.
+          </div>
+          <Button
+            onClick={() => {
+              setCurrentStep(currentStep + 1)
+            }}
+            className="welcome-screen-button mx-auto"
           >
-            <h1>Welcome!</h1>
-            <div>
-              You can create a new profile or import an already existing one.
-            </div>
-            <Button
-              onClick={() => {
-                setCurrentStep(currentStep + 1)
-              }}
-              className="welcome-screen-button mx-auto"
-            >
-              Create a new profile
-            </Button>
-            <Button className="welcome-screen-button mx-auto">
-              Import an existing profile
-            </Button>
-          </Stack>
-        )}
+            Create a new profile
+          </Button>
+          <Button className="welcome-screen-button mx-auto">
+            Import an existing profile
+          </Button>
+        </Stack>
+      )}
 
-        {currentStep === 1 && (
-          <SelectAllergens
-            allergenArray={allergenArray}
-            currentStep={currentStep}
-            selectedAllergens={selectedAllergens}
-            setSelectedAllergens={setSelectedAllergens}
-          />
-        )}
+      {currentStep === 1 && (
+        <SelectAllergens
+          allergenArray={allergenArray}
+          currentStep={currentStep}
+          selectedAllergens={selectedAllergens}
+          setSelectedAllergens={setSelectedAllergens}
+        />
+      )}
 
-        {currentStep === 2 && (
-          <SelectDiets
-            currentStep={currentStep}
-            selectedDiets={selectedDiets}
-            setSelectedDiets={setSelectedDiets}
-          />
-        )}
+      {currentStep === 2 && (
+        <SelectDiets
+          currentStep={currentStep}
+          selectedDiets={selectedDiets}
+          setSelectedDiets={setSelectedDiets}
+        />
+      )}
 
-        {currentStep === 3 && (
-          <SelectTastePreferences
-            currentStep={currentStep}
-            selectedTastePreferences={selectedTastePreferences}
-            setSelectedTastePreferences={setSelectedTastePreferences}
-          />
-        )}
-      </div>
+      {currentStep === 3 && (
+        <SelectTastePreferences
+          currentStep={currentStep}
+          selectedTastePreferences={selectedTastePreferences}
+          setSelectedTastePreferences={setSelectedTastePreferences}
+        />
+      )}
     </>
   )
 }
