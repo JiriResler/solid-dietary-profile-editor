9e3e7585d880316e807f6be9c14a62ec698be051
JiriResler <j.resler.29@gmail.com>
2024-05-10 18:28:25 +0200

    make select provider stack components have different heights based on screen size

--- src/components/Login/LoginScreen.tsx
+++ src/components/Login/LoginScreen.tsx
@@ -10,6 +10,7 @@ import Card from 'react-bootstrap/Card'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import { useState } from 'react'
+import Container from 'react-bootstrap/Container'
 
 const LoginScreen: React.FC = () => {
   const { language, setLanguage } = useContext(LanguageContext)
@@ -66,79 +67,83 @@ const LoginScreen: React.FC = () => {
         </Modal.Footer>
       </Modal>
 
-      <Row className="d-none d-lg-flex">
-        <Col md={7} className="position-relative">
-          <Stack className="w-50 text-center position-absolute top-50 start-50 translate-middle">
-            <h2>Welcome to the personal eating preferences profile editor!</h2>
-            <img
-              className="application-logo"
-              src="images/app_logo.svg"
-              alt="application_logo"
-            />
+      <Container fluid>
+        <Row className="login-screen-row d-none d-lg-flex">
+          <Col md={7} className="position-relative">
+            <Stack className="w-50 text-center position-absolute top-50 start-50 translate-middle">
+              <h2>
+                Welcome to the personal eating preferences profile editor!
+              </h2>
+              <img
+                className="application-logo"
+                src="images/app_logo.svg"
+                alt="application_logo"
+              />
 
-            <h4 className="mt-3 w-50 mx-auto">
-              Manage your eating preferences profile
-            </h4>
-          </Stack>
-          <div className="position-absolute bottom-0 start-50 translate-middle-x mb-3">
-            <span
-              className="clickable-text"
-              onClick={() => setShowAboutModal(true)}
-            >
-              About
-            </span>
-            <span> | </span>
-            <a
-              href="https://github.com/JiriResler/solid-dietary-profile-editor"
-              target="_blank"
-              className="link-without-decoration"
-            >
-              Source code
-            </a>
-            <span> | </span>
-            <span>Created by Jiří Resler</span>
-          </div>
-        </Col>
-
-        <Col md={5} className="select-provider-login-col">
-          <Card className="select-provider-card mx-auto pb-1">
-            <Card.Body>
-              <Stack
-                gap={3}
-                className="select-provider-stack text-center mx-auto"
+              <h4 className="mt-3 w-50 mx-auto">
+                Manage your eating preferences profile
+              </h4>
+            </Stack>
+            <div className="position-absolute bottom-0 start-50 translate-middle-x mb-3">
+              <span
+                className="clickable-text"
+                onClick={() => setShowAboutModal(true)}
               >
-                <SelectProvider />
-              </Stack>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+                About
+              </span>
+              <span> | </span>
+              <a
+                href="https://github.com/JiriResler/solid-dietary-profile-editor"
+                target="_blank"
+                className="link-without-decoration"
+              >
+                Source code
+              </a>
+              <span> | </span>
+              <span>Created by Jiří Resler</span>
+            </div>
+          </Col>
 
-      <Stack
-        gap={3}
-        className="select-provider-stack position-absolute top-50 start-50 translate-middle pb-5 d-lg-none text-center mx-auto"
-      >
-        <img
-          className="application-logo"
-          src="images/app_logo.svg"
-          alt="application_logo"
-        />
+          <Col md={5} className="select-provider-login-col">
+            <Card className="select-provider-card mx-auto pb-1">
+              <Card.Body>
+                <Stack
+                  gap={3}
+                  className="select-provider-stack select-provider-stack-large-screen text-center mx-auto"
+                >
+                  <SelectProvider />
+                </Stack>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
 
-        <h5>Manage your personal eating preferences profile</h5>
-
-        <SelectProvider />
-      </Stack>
-
-      <div className="position-absolute bottom-0 start-0 ms-3 mb-3">
-        <Form.Select
-          value={languageFullName(language)}
-          onChange={(e) => setLanguageShorthand(e.target.value)}
+        <Stack
+          gap={3}
+          className="select-provider-stack select-provider-stack-small-screen position-absolute top-50 start-50 translate-middle pb-5 d-lg-none text-center mx-auto"
         >
-          <option key="en">English</option>
-          <option key="sk">Slovensky</option>
-          <option key="cs">Česky</option>
-        </Form.Select>
-      </div>
+          <img
+            className="application-logo"
+            src="images/app_logo.svg"
+            alt="application_logo"
+          />
+
+          <h5>Manage your personal eating preferences profile</h5>
+
+          <SelectProvider />
+        </Stack>
+
+        <div className="position-absolute bottom-0 start-0 ms-3 mb-3">
+          <Form.Select
+            value={languageFullName(language)}
+            onChange={(e) => setLanguageShorthand(e.target.value)}
+          >
+            <option key="en">English</option>
+            <option key="sk">Slovensky</option>
+            <option key="cs">Česky</option>
+          </Form.Select>
+        </div>
+      </Container>
     </>
   )
 }
