47ff0132d65d6b7365c848aa9e70324c37ae8468
JiriResler <j.resler.29@gmail.com>
2024-05-15 19:04:30 +0200

    read allergen URLs from list of common allergens

--- src/components/Profile/CreateProfile/loadProfileCreationData.ts
+++ src/components/Profile/CreateProfile/loadProfileCreationData.ts
@@ -1,48 +1,66 @@
-// import { fetch } from '@inrupt/solid-client-authn-browser'
 import {
-  // getSolidDataset,
-  // getThing,
+  getSolidDataset,
+  getThing,
   getUrl,
-  // getStringEnglish,
-  // getInteger,
+  getStringEnglish,
+  getInteger,
+  fromRdfJsDataset,
 } from '@inrupt/solid-client'
-// import { Allergen } from './profileDataTypes'
-import { fromRdfJsDataset, getThing } from '@inrupt/solid-client'
+import { Allergen } from './profileDataTypes'
 import N3 from 'n3'
 
 export async function loadAllergenList() {
-  const ontologyUrl =
-    'https://raw.githubusercontent.com/JiriResler/personalized-restaurant-menu-viewer-application-ontology/main/ontology.ttl'
+  const allergenListFileUrl =
+    'https://raw.githubusercontent.com/JiriResler/personalized-restaurant-menu-viewer-application-ontology/main/resource/List_of_most_common_allergens.ttl'
 
-  const response = await window.fetch(ontologyUrl)
+  const fetchedResponse = await fetch(allergenListFileUrl)
 
-  const responseText = await response.text()
+  const allergenListInTurtle = await fetchedResponse.text()
 
   const parser = new N3.Parser()
 
-  const store = new N3.Store()
-
   try {
-    const parsed = parser.parse(responseText)
+    const store = new N3.Store(parser.parse(allergenListInTurtle))
 
-    for (const { subject, predicate, object, graph } of parsed) {
-      store.addQuad(subject, predicate, object, graph)
-    }
-
-    const solidDataset = fromRdfJsDataset(store)
+    // Create a solid dataset from parsed store
+    const listAsRdfDataset = fromRdfJsDataset(store)
 
     const listOfAllergens = getThing(
-      solidDataset,
-      'https://raw.githubusercontent.com/JiriResler/personalized-restaurant-menu-viewer-application-ontology/main/ontology#List_of_most_common_allergens',
+      listAsRdfDataset,
+      'https://raw.githubusercontent.com/JiriResler/personalized-restaurant-menu-viewer-application-ontology/main/resource/List_of_most_common_allergens',
     )
 
     if (listOfAllergens === null) {
       return
     }
 
-    const listUrl = getUrl(listOfAllergens, 'https://schema.org/url')
+    // Get sequence from a thing
+    const allergenUrlArray = []
+    let counter = 1
 
-    console.log(listUrl)
+    const sequenceContainsItemPredicate =
+      'http://www.w3.org/1999/02/22-rdf-syntax-ns#_'
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const sequenceContainsItemPredicateFull =
+        sequenceContainsItemPredicate + counter
+
+      const allergenUrl = getUrl(
+        listOfAllergens,
+        sequenceContainsItemPredicateFull,
+      )
+
+      if (allergenUrl === null) {
+        break
+      }
+
+      allergenUrlArray.push(allergenUrl)
+
+      counter = counter + 1
+    }
+
+    console.log(allergenUrlArray)
   } catch (error) {
     console.log(error)
   }
@@ -73,102 +91,102 @@ export async function loadAllergenList() {
 //   await fetchAllergenArray(setAllergenArray)
 // }
 
-// async function fetchAllergenArray(
-//   setAllergenArray: React.Dispatch<React.SetStateAction<Allergen[]>>,
-// ) {
-//   const dataset = await getSolidDataset(
-//     'https://personal-restaurant-menu-viewer-app.solidcommunity.net/public/resource/List_of_allergens.ttl',
-//     {
-//       fetch: fetch as undefined,
-//     },
-//   )
+async function fetchAllergenArray(
+  setAllergenArray: React.Dispatch<React.SetStateAction<Allergen[]>>,
+) {
+  const dataset = await getSolidDataset(
+    'https://personal-restaurant-menu-viewer-app.solidcommunity.net/public/resource/List_of_allergens.ttl',
+    {
+      fetch: fetch as undefined,
+    },
+  )
 
-//   const thing = getThing(
-//     dataset,
-//     'https://personal-restaurant-menu-viewer-app.solidcommunity.net/public/resource/List_of_allergens',
-//   )
+  const thing = getThing(
+    dataset,
+    'https://personal-restaurant-menu-viewer-app.solidcommunity.net/public/resource/List_of_allergens',
+  )
 
-//   if (thing === null) {
-//     return
-//   }
+  if (thing === null) {
+    return
+  }
 
-//   // get sequence from thing
-//   let counter = 1
+  // get sequence from thing
+  let counter = 1
 
-//   let itemExists = true
+  let itemExists = true
 
-//   const containsPropertyUrl = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#_'
+  const containsPropertyUrl = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#_'
 
-//   const allergenUrlArray = []
+  const allergenUrlArray = []
 
-//   while (itemExists) {
-//     const propertyUrl = containsPropertyUrl + counter
+  while (itemExists) {
+    const propertyUrl = containsPropertyUrl + counter
 
-//     const allergenUrl = getUrl(thing, propertyUrl)
+    const allergenUrl = getUrl(thing, propertyUrl)
 
-//     if (allergenUrl === null) {
-//       itemExists = false
-//       break
-//     }
+    if (allergenUrl === null) {
+      itemExists = false
+      break
+    }
 
-//     allergenUrlArray.push(allergenUrl)
+    allergenUrlArray.push(allergenUrl)
 
-//     counter = counter + 1
-//   }
+    counter = counter + 1
+  }
 
-//   // Load allergens
-//   const allergenArray: Allergen[] = []
+  // Load allergens
+  const allergenArray: Allergen[] = []
 
-//   for (const allergenUrl of allergenUrlArray) {
-//     const allergenDataset = await getSolidDataset(allergenUrl + '.ttl', {
-//       fetch: fetch as undefined,
-//     })
+  for (const allergenUrl of allergenUrlArray) {
+    const allergenDataset = await getSolidDataset(allergenUrl + '.ttl', {
+      fetch: fetch as undefined,
+    })
 
-//     const allergenThing = getThing(allergenDataset, allergenUrl)
+    const allergenThing = getThing(allergenDataset, allergenUrl)
 
-//     if (allergenThing === null) {
-//       return
-//     }
+    if (allergenThing === null) {
+      return
+    }
 
-//     const allergenNumber = getInteger(
-//       allergenThing,
-//       'https://personal-restaurant-menu-viewer-app.solidcommunity.net/public/ontology#allergenNumber',
-//     )
+    const allergenNumber = getInteger(
+      allergenThing,
+      'https://personal-restaurant-menu-viewer-app.solidcommunity.net/public/ontology#allergenNumber',
+    )
 
-//     const allergenLabel = getStringEnglish(
-//       allergenThing,
-//       'http://www.w3.org/2000/01/rdf-schema#label',
-//     )
+    const allergenLabel = getStringEnglish(
+      allergenThing,
+      'http://www.w3.org/2000/01/rdf-schema#label',
+    )
 
-//     const allergenIconUrl = getUrl(
-//       allergenThing,
-//       'https://personal-restaurant-menu-viewer-app.solidcommunity.net/public/ontology#hasIcon',
-//     )
+    const allergenIconUrl = getUrl(
+      allergenThing,
+      'https://personal-restaurant-menu-viewer-app.solidcommunity.net/public/ontology#hasIcon',
+    )
 
-//     const allergenSameAsIri = getUrl(
-//       allergenThing,
-//       'http://www.w3.org/2002/07/owl#sameAs',
-//     )
+    const allergenSameAsIri = getUrl(
+      allergenThing,
+      'http://www.w3.org/2002/07/owl#sameAs',
+    )
 
-//     if (
-//       allergenNumber === null ||
-//       allergenLabel === null ||
-//       allergenIconUrl === null ||
-//       allergenSameAsIri === null
-//     ) {
-//       return
-//     }
+    if (
+      allergenNumber === null ||
+      allergenLabel === null ||
+      allergenIconUrl === null ||
+      allergenSameAsIri === null
+    ) {
+      return
+    }
 
-//     const allergen: Allergen = {
-//       IRI: allergenUrl,
-//       label: allergenLabel,
-//       menuLegendNumber: allergenNumber,
-//       iconUrl: allergenIconUrl,
-//       sameAsIri: allergenSameAsIri,
-//     }
+    const allergen: Allergen = {
+      IRI: allergenUrl,
+      label: allergenLabel,
+      menuLegendNumber: allergenNumber,
+      iconUrl: allergenIconUrl,
+      sameAsIri: allergenSameAsIri,
+    }
 
-//     allergenArray.push(allergen)
-//   }
+    allergenArray.push(allergen)
+  }
 
-//   setAllergenArray(allergenArray)
-// }
+  setAllergenArray(allergenArray)
+}
