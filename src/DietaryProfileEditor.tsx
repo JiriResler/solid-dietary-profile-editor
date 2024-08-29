68219ad42c4621bd8cc46e30fd34de46bd6a27b4
JiriResler <j.resler.29@gmail.com>
2024-08-29 14:46:35 +0200

    move routing to top app component

--- src/DietaryProfileEditor.tsx
+++ src/DietaryProfileEditor.tsx
@@ -1,12 +1,31 @@
 import { SessionProvider } from '@inrupt/solid-ui-react'
 import IntlProviderWrapper from './components/IntlProviderWrapper'
-import DietaryProfileEditorRouter from './components/DietaryProfileEditorRouter'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Login/Login'
+import ProfileManagement from './components/ProfileManagement/ProfileManagement'
+import RequireAuth from './components/RequireAuth'
 
 const DietaryProfileEditor: React.FC = () => {
   return (
     <SessionProvider>
       <IntlProviderWrapper>
-        <DietaryProfileEditorRouter />
+        <BrowserRouter
+          basename={
+            import.meta.env.DEV ? '/' : '/solid-dietary-profile-editor/'
+          }
+        >
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <ProfileManagement />
+                </RequireAuth>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
       </IntlProviderWrapper>
     </SessionProvider>
   )
