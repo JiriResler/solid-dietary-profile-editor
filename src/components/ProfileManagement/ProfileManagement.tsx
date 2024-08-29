4645c9d490fe2dc02c4cf655c4193d531e546a6e
JiriResler <j.resler.29@gmail.com>
2024-08-29 16:12:27 +0200

    add sign out button to profile management component

--- src/components/ProfileManagement/ProfileManagement.tsx
+++ src/components/ProfileManagement/ProfileManagement.tsx
@@ -2,6 +2,9 @@ import { useSession } from '@inrupt/solid-ui-react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { Navigate } from 'react-router-dom'
 import { auth } from '../../firebase'
+import { LoginMethod } from '../loginMethodEnum'
+import { signOut } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 const ProfileManagement: React.FC = () => {
   const { session: solidSession } = useSession()
@@ -10,14 +13,43 @@ const ProfileManagement: React.FC = () => {
 
   const authed = solidSession.info.isLoggedIn || firebaseUser !== null
 
+  const navigate = useNavigate()
+
   if (!authed) {
     return <Navigate to="/login" replace />
   }
 
+  const loginMethod: LoginMethod = solidSession.info.isLoggedIn
+    ? LoginMethod.SOLID
+    : LoginMethod.FIREBASE
+
   return (
     <>
       <h1>Congrats, you are authenticated.</h1>
-      <button>Sign out</button>
+      <h2>
+        Authentication method:{' '}
+        {loginMethod === LoginMethod.SOLID ? 'Solid' : 'Firebase'}
+      </h2>
+      <button
+        onClick={() => {
+          if (loginMethod === LoginMethod.SOLID) {
+            solidSession
+              .logout({ logoutType: 'app' })
+              .then(() => navigate('/login'))
+              .catch((error: Error) => {
+                console.log(error.message)
+              })
+          } else {
+            signOut(auth)
+              .then(() => navigate('/login'))
+              .catch((error: Error) => {
+                console.log(error.message)
+              })
+          }
+        }}
+      >
+        Sign out
+      </button>
     </>
   )
 }
