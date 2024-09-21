2ef0bd7422b5ea813258eb61d318b801ba837f77
Jiri Resler <j.resler.29@gmail.com>
2024-09-21 10:27:46 +0200

    update loginSolidHelpers test cases, add a new test case

--- src/components/Login/loginSolidHelpers.test.ts
+++ src/components/Login/loginSolidHelpers.test.ts
@@ -1,14 +1,17 @@
-import { describe, test, expect } from 'vitest'
+import { describe, test, expect, beforeEach } from 'vitest'
 import { getKeyByValue } from './loginSolidHelpers'
 
-describe('loginSolidHelpers', () => {
-  test('return existing key', () => {
-    const obj = {
+describe('loginSolidHelper functions', () => {
+  let obj: object
+
+  beforeEach(() => {
+    obj = {
       testKey: 'testValue',
     }
+  })
 
+  test('should return the existing key for a valid value', () => {
     const value = 'testValue'
-
     const expectedKey = 'testKey'
 
     const actualKey = getKeyByValue(obj, value)
@@ -16,16 +19,21 @@ describe('loginSolidHelpers', () => {
     expect(actualKey).toEqual(expectedKey)
   })
 
-  test('return non existing key', () => {
-    const obj = {
-      testKey: 'testValue',
-    }
-
+  test('should return undefined for a non-existing value', () => {
     const value = 'nonExistingValue'
-
     const expectedKey = undefined
 
     const actualKey = getKeyByValue(obj, value)
+
+    expect(actualKey).toEqual(expectedKey)
+  })
+
+  test('should return undefined if the object is empty', () => {
+    const emptyObj = {}
+    const value = 'testValue'
+    const expectedKey = undefined
+
+    const actualKey = getKeyByValue(emptyObj, value)
 
     expect(actualKey).toEqual(expectedKey)
   })
