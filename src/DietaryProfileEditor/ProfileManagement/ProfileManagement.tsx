8b13efbc29fcc9a03215a471e0e21733427b12a1
JiriResler <j.resler.29@gmail.com>
2024-11-15 12:10:37 +0100

    temporarily remove authentication check from ProfileManagement component

--- src/DietaryProfileEditor/ProfileManagement/ProfileManagement.tsx
+++ src/DietaryProfileEditor/ProfileManagement/ProfileManagement.tsx
@@ -1,55 +1,7 @@
-import { useSession } from '@inrupt/solid-ui-react'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { Navigate } from 'react-router-dom'
-import { auth } from '../../firebase'
-import { LoginMethod } from '../loginMethodEnum'
-import { signOut } from 'firebase/auth'
-import { useNavigate } from 'react-router-dom'
-
 const ProfileManagement: React.FC = () => {
-  const { session: solidSession } = useSession()
-
-  const [firebaseUser] = useAuthState(auth)
-
-  const authed = solidSession.info.isLoggedIn || firebaseUser !== null
-
-  const navigate = useNavigate()
-
-  if (!authed) {
-    return <Navigate to="/login" replace />
-  }
-
-  const loginMethod: LoginMethod = solidSession.info.isLoggedIn
-    ? LoginMethod.SOLID
-    : LoginMethod.FIREBASE
-
   return (
     <>
-      <h1>Congrats, you are authenticated.</h1>
-      <h2>
-        Authentication method:{' '}
-        {loginMethod === LoginMethod.SOLID ? 'Solid' : 'Firebase'}
-      </h2>
-      <button
-        onClick={() => {
-          if (loginMethod === LoginMethod.SOLID) {
-            solidSession
-              .logout({ logoutType: 'app' })
-              .then(() => navigate('/login'))
-              .catch((error: Error) => {
-                console.log(error.message)
-              })
-          } else {
-            signOut(auth)
-              .then(() => navigate('/login'))
-              .catch((error: Error) => {
-                console.log(error.message)
-              })
-          }
-        }}
-      >
-        Sign out
-      </button>
+      <h1>Profile Management</h1>
     </>
   )
 }
