4c67bff1a7f37ca8f95d49fb9cb71615880f1a8d
JiriResler <j.resler.29@gmail.com>
2024-11-22 17:11:59 +0100

    add a Stack and a Stepper to the EditDietaryProfile component

--- src/DietaryProfileEditor/DietaryProfileManagement/EditDietaryProfile/EditDietaryProfile.tsx
+++ src/DietaryProfileEditor/DietaryProfileManagement/EditDietaryProfile/EditDietaryProfile.tsx
@@ -1,5 +1,88 @@
+import Stack from 'react-bootstrap/Stack'
+import './EditDietaryProfile.css'
+import Stepper from '@mui/material/Stepper'
+import Step from '@mui/material/Step'
+import StepLabel from '@mui/material/StepLabel'
+import { useState } from 'react'
+import { FormattedMessage } from 'react-intl'
+import { styled } from '@mui/material/styles'
+import StepConnector, {
+  stepConnectorClasses,
+} from '@mui/material/StepConnector'
+
 const EditDietaryProfile: React.FC = () => {
-  return <h1>Edit profile</h1>
+  const [currentStep] = useState(0)
+
+  const CustomStepConnector = styled(StepConnector)(() => ({
+    [`&.${stepConnectorClasses.alternativeLabel}`]: {
+      top: 10,
+      left: 'calc(-50% + 12px)',
+      right: 'calc(50% + 12px)',
+    },
+    [`&.${stepConnectorClasses.active}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: '#1c3bb8',
+      },
+    },
+    [`&.${stepConnectorClasses.completed}`]: {
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: '#1c3bb8',
+      },
+    },
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: '#eaeaf0',
+      borderTopWidth: 3,
+      borderRadius: 1,
+    },
+  }))
+
+  return (
+    <Stack gap={3} className="edit-profile-stack position-relative text-center">
+      <Stepper
+        activeStep={currentStep}
+        alternativeLabel
+        className="edit-profile-stepper mt-3"
+        connector={<CustomStepConnector />}
+        sx={{
+          '& .MuiStepLabel-iconContainer.Mui-active > .MuiSvgIcon-root.Mui-active':
+            {
+              color: '#2541b2',
+            },
+          '& .MuiStepLabel-iconContainer.Mui-completed > .MuiSvgIcon-root.Mui-completed':
+            {
+              color: '#2541b2',
+            },
+          '& .MuiStepLabel-labelContainer': {
+            height: '50px',
+            position: 'relative',
+          },
+        }}
+      >
+        <Step key="step1">
+          <StepLabel>
+            <FormattedMessage id="allergens" defaultMessage="Allergens" />
+          </StepLabel>
+        </Step>
+
+        <Step key="step2">
+          <StepLabel>
+            <FormattedMessage id="diets" defaultMessage="Diets" />
+          </StepLabel>
+        </Step>
+
+        <Step key="step3">
+          <StepLabel>
+            <div className="position-absolute top-50 start-50 translate-middle">
+              <FormattedMessage
+                id="tastePreferences"
+                defaultMessage="Taste preferences"
+              />
+            </div>
+          </StepLabel>
+        </Step>
+      </Stepper>
+    </Stack>
+  )
 }
 
 export default EditDietaryProfile
