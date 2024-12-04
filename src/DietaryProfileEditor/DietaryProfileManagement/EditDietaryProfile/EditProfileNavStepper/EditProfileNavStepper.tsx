537d34125471756b7a7520e5311afe9eb173857b
JiriResler <j.resler.29@gmail.com>
2024-12-04 14:46:18 +0100

    add a second progress component to display full circle of the main progress component

--- src/DietaryProfileEditor/DietaryProfileManagement/EditDietaryProfile/EditProfileNavStepper/EditProfileNavStepper.tsx
+++ src/DietaryProfileEditor/DietaryProfileManagement/EditDietaryProfile/EditProfileNavStepper/EditProfileNavStepper.tsx
@@ -14,8 +14,30 @@ function CircularProgressWithLabel(
         variant="determinate"
         size="60px"
         thickness={6}
-        {...props}
+        value={100}
+        sx={{ color: 'gray' }}
       />
+
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <CircularProgress
+          variant="determinate"
+          size="60px"
+          thickness={6}
+          {...props}
+        />
+      </Box>
+
       <Box
         sx={{
           top: 0,
