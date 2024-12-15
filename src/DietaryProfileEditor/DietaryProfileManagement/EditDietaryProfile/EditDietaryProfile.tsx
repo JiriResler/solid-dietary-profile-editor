f4d470b8123febe4cc93da66e901d32b252d940c
Jiri Resler <j.resler.29@gmail.com>
2024-12-15 12:46:46 +0100

    add scrolling capabilities to large screens in edit profile

--- src/DietaryProfileEditor/DietaryProfileManagement/EditDietaryProfile/EditDietaryProfile.tsx
+++ src/DietaryProfileEditor/DietaryProfileManagement/EditDietaryProfile/EditDietaryProfile.tsx
@@ -1,11 +1,11 @@
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import './EditDietaryProfile.css'
 import EditProfileNavStepper from './EditProfileNavStepper/EditProfileNavStepper'
 import EditProfileNavButtons from './EditProfileNavButtons/EditProfileNavButtons'
 import Card from 'react-bootstrap/Card'
 import AllergensAndIntolerances from './AllergensAndIntolerances/AllergensAndIntolerances'
 import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 /**
  * Renders the edit profile screen with its header, main content and footer.
@@ -41,15 +41,17 @@ const EditDietaryProfile: React.FC = () => {
         <EditProfileScreen />
       </div>
 
-      <Row className="d-none d-lg-block position-relative edit-profile-large-screen h-100">
-        <Col className="">
-          <Card className="edit-profile-card position-absolute top-50 start-50 translate-middle">
-            <Card.Body>
-              <EditProfileScreen />
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+      <div className="d-none d-lg-block h-100">
+        <Row className="background-primary-color position-relative h-100">
+          <Col>
+            <Card className="edit-profile-card position-absolute top-50 start-50 translate-middle">
+              <Card.Body className="h-100">
+                <EditProfileScreen />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </div>
   )
 }
