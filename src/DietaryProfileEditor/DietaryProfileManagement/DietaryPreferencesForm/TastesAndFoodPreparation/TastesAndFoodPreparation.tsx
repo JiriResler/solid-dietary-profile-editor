7efde7acea15208419eb36ba54d76f3b63b87aeb
JiriResler <j.resler.29@gmail.com>
2025-01-11 15:49:14 +0100

    create TastesAndFoodPreparation component

---
+++ src/DietaryProfileEditor/DietaryProfileManagement/DietaryPreferencesForm/TastesAndFoodPreparation/TastesAndFoodPreparation.tsx
@@ -0,0 +1,14 @@
+import './TastesAndFoodPreparation.css'
+
+/**
+ *
+ */
+const TastesAndFoodPreparation: React.FC = () => {
+  return (
+    <>
+      <h1>hello from TastesAndFoodPreparation</h1>
+    </>
+  )
+}
+
+export default TastesAndFoodPreparation
