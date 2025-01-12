8c32861654ae0a6acf59d4d3d1e773a94fdd84e3
JiriResler <j.resler.29@gmail.com>
2025-01-12 13:32:28 +0100

    add favorite world cuisine checkboxes to TastesAndFoodPreparation

--- src/DietaryProfileEditor/DietaryProfileManagement/DietaryPreferencesForm/TastesAndFoodPreparation/TastesAndFoodPreparation.tsx
+++ src/DietaryProfileEditor/DietaryProfileManagement/DietaryPreferencesForm/TastesAndFoodPreparation/TastesAndFoodPreparation.tsx
@@ -1,10 +1,27 @@
 import './TastesAndFoodPreparation.css'
 import { FormattedMessage } from 'react-intl'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Stack from 'react-bootstrap/Stack'
+import FormCheckbox from '../FormCheckbox/FormCheckbox'
 
 /**
  *
  */
 const TastesAndFoodPreparation: React.FC = () => {
+  const popularCuisines = [
+    'Italian',
+    'Indian',
+    'French',
+    'Chinese',
+    'Middle',
+    'Spanish',
+    'Japanese',
+    'Thai',
+    'Mexican',
+    'Korean',
+  ]
+
   return (
     <>
       <div className="form-group-heading">
@@ -13,6 +30,24 @@ const TastesAndFoodPreparation: React.FC = () => {
           defaultMessage="What are your favorite world cuisines?"
         />
       </div>
+
+      <Row className="ms-auto">
+        <Col xs={6} lg={3}>
+          <Stack gap={2}>
+            {popularCuisines.slice(0, 5).map((cuisine) => {
+              return <FormCheckbox label={cuisine} key={cuisine} />
+            })}
+          </Stack>
+        </Col>
+
+        <Col xs={6} lg={3}>
+          <Stack gap={2}>
+            {popularCuisines.slice(5, 10).map((cuisine) => {
+              return <FormCheckbox label={cuisine} key={cuisine} />
+            })}
+          </Stack>
+        </Col>
+      </Row>
     </>
   )
 }
