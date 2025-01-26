0eacc6f7838cd0e64ab6275cf28a8587b43cce55
Jiri Resler <j.resler.29@gmail.com>
2025-01-26 09:42:29 +0100

    Send user to login screen when they are not authenticated

--- src/DietaryProfileEditor/DietaryProfileManagement/DietaryProfileManagement.tsx
+++ src/DietaryProfileEditor/DietaryProfileManagement/DietaryProfileManagement.tsx
@@ -2,9 +2,23 @@ import React, { useState } from 'react'
 import ProfileOverview from './ProfileOverview/ProfileOverview'
 import './DietaryProfileManagement.css'
 import DietaryPreferencesForm from './DietaryPreferencesForm/DietaryPreferencesForm'
+import { useSession } from '@inrupt/solid-ui-react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../../firebase'
+import { Navigate } from 'react-router-dom'
 
 const DietaryProfileManagement: React.FC = () => {
+  const { session: solidSession } = useSession()
+
+  const [firebaseUser] = useAuthState(auth)
+
+  const authed = solidSession.info.isLoggedIn || firebaseUser !== null
+
   const [userProfileExists] = useState(true)
+
+  if (!authed) {
+    return <Navigate to="/login" replace />
+  }
 
   if (userProfileExists) {
     return <ProfileOverview />
