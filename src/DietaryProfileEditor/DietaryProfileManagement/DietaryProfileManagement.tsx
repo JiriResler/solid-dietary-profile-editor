25f0c6784cedb43a75e53336601f3a0a650542b0
JiriResler <j.resler.29@gmail.com>
2025-01-29 15:13:26 +0100

    Display profile overview screen upon user login

--- src/DietaryProfileEditor/DietaryProfileManagement/DietaryProfileManagement.tsx
+++ src/DietaryProfileEditor/DietaryProfileManagement/DietaryProfileManagement.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 import ProfileOverview from './ProfileOverview/ProfileOverview'
 import './DietaryProfileManagement.css'
-import DietaryPreferencesForm from './DietaryPreferencesForm/DietaryPreferencesForm'
+// import DietaryPreferencesForm from './DietaryPreferencesForm/DietaryPreferencesForm'
 import { useSession } from '@inrupt/solid-ui-react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from '../../firebase'
@@ -14,17 +14,11 @@ const DietaryProfileManagement: React.FC = () => {
 
   const authed = solidSession.info.isLoggedIn || firebaseUser !== null
 
-  const [userProfileExists] = useState(true)
-
   if (!authed) {
     return <Navigate to="/login" replace />
   }
 
-  if (userProfileExists) {
-    return <ProfileOverview />
-  } else {
-    return <DietaryPreferencesForm />
-  }
+  return <ProfileOverview />
 }
 
 export default DietaryProfileManagement
