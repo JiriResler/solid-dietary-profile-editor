e959d9afdcaa742bfda92395eb0ed13ddda69b21
JiriResler <j.resler.29@gmail.com>
2025-07-13 18:41:23 +0200

    Add rest of diet checkboxes

--- src/DietaryProfileEditor/DietaryProfileManagement/DietaryPreferencesForm/SelectDietPreferences/SelectDietPreferences.tsx
+++ src/DietaryProfileEditor/DietaryProfileManagement/DietaryPreferencesForm/SelectDietPreferences/SelectDietPreferences.tsx
@@ -22,7 +22,14 @@ const SelectDietPreferences: React.FC<SelectDietPreferencesProps> = ({
   const intl = useIntl()
 
   const dietIriList = {
-    vegetarian: 'http://www.wikidata.org/entity/Q83364',
+    vegetarianDiet: 'http://www.wikidata.org/entity/Q83364',
+    mediterraneanDiet: 'http://www.wikidata.org/entity/Q47564',
+    lowCarbDiet: 'http://www.wikidata.org/entity/Q1570280',
+    dashDiet: 'http://www.wikidata.org/entity/Q5204325',
+    veganDiet: 'http://www.wikidata.org/entity/Q181138',
+    ketoDiet: 'http://www.wikidata.org/entity/Q1070684',
+    atkinsDiet: 'http://www.wikidata.org/entity/Q756030',
+    paleoDiet: 'http://www.wikidata.org/entity/Q533945',
   }
 
   /**
@@ -45,17 +52,15 @@ const SelectDietPreferences: React.FC<SelectDietPreferencesProps> = ({
         />
       </div>
 
-      <span>Selected diets: {selectedDiets}</span>
-
       <Row className="ms-auto">
         <Col xs={6} lg={4}>
           <Stack gap={2}>
             <Form.Check id={'vegetarian-diet-checkbox'}>
               <Form.Check.Input
                 className="app-form-control app-form-checkbox"
-                checked={selectedDiets.includes(dietIriList['vegetarian'])}
+                checked={selectedDiets.includes(dietIriList['vegetarianDiet'])}
                 onChange={() =>
-                  handleDietCheckboxOnChange(dietIriList['vegetarian'])
+                  handleDietCheckboxOnChange(dietIriList['vegetarianDiet'])
                 }
               />
 
@@ -66,11 +71,115 @@ const SelectDietPreferences: React.FC<SelectDietPreferencesProps> = ({
                 />
               </Form.Check.Label>
             </Form.Check>
+
+            <Form.Check id={'mediterranean-diet-checkbox'}>
+              <Form.Check.Input
+                className="app-form-control app-form-checkbox"
+                checked={selectedDiets.includes(
+                  dietIriList['mediterraneanDiet'],
+                )}
+                onChange={() =>
+                  handleDietCheckboxOnChange(dietIriList['mediterraneanDiet'])
+                }
+              />
+
+              <Form.Check.Label className="ms-2">
+                <FormattedMessage
+                  id={'mediterraneanDiet'}
+                  defaultMessage={'Mediterranean'}
+                />
+              </Form.Check.Label>
+            </Form.Check>
+
+            <Form.Check id={'low-carb-diet-checkbox'}>
+              <Form.Check.Input
+                className="app-form-control app-form-checkbox"
+                checked={selectedDiets.includes(dietIriList['lowCarbDiet'])}
+                onChange={() =>
+                  handleDietCheckboxOnChange(dietIriList['lowCarbDiet'])
+                }
+              />
+
+              <Form.Check.Label className="ms-2">
+                <FormattedMessage
+                  id={'lowCarbDiet'}
+                  defaultMessage={'Low-carbohydrate'}
+                />
+              </Form.Check.Label>
+            </Form.Check>
+
+            <Form.Check id={'dash-diet-checkbox'}>
+              <Form.Check.Input
+                className="app-form-control app-form-checkbox"
+                checked={selectedDiets.includes(dietIriList['dashDiet'])}
+                onChange={() =>
+                  handleDietCheckboxOnChange(dietIriList['dashDiet'])
+                }
+              />
+
+              <Form.Check.Label className="ms-2">DASH</Form.Check.Label>
+            </Form.Check>
           </Stack>
         </Col>
 
         <Col xs={6} lg={4}>
-          <Stack gap={2}></Stack>
+          <Stack gap={2}>
+            <Form.Check id={'vegan-diet-checkbox'}>
+              <Form.Check.Input
+                className="app-form-control app-form-checkbox"
+                checked={selectedDiets.includes(dietIriList['veganDiet'])}
+                onChange={() =>
+                  handleDietCheckboxOnChange(dietIriList['veganDiet'])
+                }
+              />
+
+              <Form.Check.Label className="ms-2">
+                <FormattedMessage id={'veganDiet'} defaultMessage={'Vegan'} />
+              </Form.Check.Label>
+            </Form.Check>
+
+            <Form.Check id={'keto-diet-checkbox'}>
+              <Form.Check.Input
+                className="app-form-control app-form-checkbox"
+                checked={selectedDiets.includes(dietIriList['ketoDiet'])}
+                onChange={() =>
+                  handleDietCheckboxOnChange(dietIriList['ketoDiet'])
+                }
+              />
+
+              <Form.Check.Label className="ms-2">
+                <FormattedMessage id={'ketoDiet'} defaultMessage={'Keto'} />
+              </Form.Check.Label>
+            </Form.Check>
+
+            <Form.Check id={'atkins-diet-checkbox'}>
+              <Form.Check.Input
+                className="app-form-control app-form-checkbox"
+                checked={selectedDiets.includes(dietIriList['atkinsDiet'])}
+                onChange={() =>
+                  handleDietCheckboxOnChange(dietIriList['atkinsDiet'])
+                }
+              />
+
+              <Form.Check.Label className="ms-2">
+                <FormattedMessage id={'atkinsDiet'} defaultMessage={'Atkins'} />
+              </Form.Check.Label>
+            </Form.Check>
+
+            <Form.Check id={'paleo-diet-checkbox'}>
+              <Form.Check.Input
+                className="app-form-control app-form-checkbox"
+                checked={selectedDiets.includes(dietIriList['paleoDiet'])}
+                onChange={() =>
+                  handleDietCheckboxOnChange(dietIriList['paleoDiet'])
+                }
+              />
+
+              <Form.Check.Label className="ms-2">
+                <FormattedMessage id={'paleoDiet'} defaultMessage={'Paleo'} />
+              </Form.Check.Label>
+            </Form.Check>
+          </Stack>
         </Col>
       </Row>
 
